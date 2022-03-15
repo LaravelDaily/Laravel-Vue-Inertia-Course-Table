07a7694846c193fb296a0c0a9cae69c14ade7f7b
PovilasKorop <povilask007@gmail.com>
2022-03-15 12:19:33 +0200

    API Resources in Inertia

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostResource;
 use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
+        $posts = PostResource::collection(Post::all());
 
         return inertia('Posts/Index', compact('posts'));
     }
