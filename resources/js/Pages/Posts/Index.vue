6bcac6fb3f178bfd4f0af7f57a8df52fa14d5c69
PovilasKorop <povilask007@gmail.com>
2022-03-16 11:24:14 +0200

    Progress indicators

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -44,6 +44,22 @@
 
 <script>
 import {Link} from "@inertiajs/inertia-vue3";
+import { InertiaProgress } from '@inertiajs/progress'
+
+InertiaProgress.init({
+    // The delay after which the progress bar will
+    // appear during navigation, in milliseconds.
+    delay: 250,
+
+    // The color of the progress bar.
+    color: '#29d',
+
+    // Whether to include the default NProgress styles.
+    includeCSS: true,
+
+    // Whether the NProgress spinner will be shown.
+    showSpinner: true,
+})
 
 export default {
     components: {
